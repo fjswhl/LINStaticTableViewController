f331ba5f4005a231170c9324e4d7886c175b3282
fjswhl <340403800@qq.com>
2015-10-03 17:49:52 +0800

    ADD: section item

--- LINStaticTableViewController/StaticTableViewController.swift
+++ LINStaticTableViewController/StaticTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// MARK: TableView Components
 struct TablViewCellItem {
     let cell: UITableViewCell
     let height: CGFloat
@@ -26,11 +27,57 @@ func == (left: TablViewCellItem, right: TablViewCellItem) -> Bool {
     return left.cell == right.cell
 }
 
+struct TableViewSectionItem {
+    var cells: [TablViewCellItem] = [TablViewCellItem]()
+
+    var headerTitle: String? = nil
+    var headerHeight: CGFloat = 0
+
+    var footerTitle: String? = nil
+    var footerHeight: CGFloat = 0
+
+    var headerView: UIView? = nil
+    var footerView: UIView? = nil
+
+    mutating func appendCellItem(cellItem: TablViewCellItem) {
+        cells.append(cellItem)
+    }
+
+    mutating func appendCellItems(cellItems: [TablViewCellItem]) {
+        cells.appendContentsOf(cellItems)
+    }
+
+    func cellCount() -> Int {
+        return cells.count
+    }
+
+    func cellItemForRow(row: Int) -> TablViewCellItem {
+        return cells[row]
+    }
+
+    func cellForRow(row: Int) -> UITableViewCell {
+        return cells[row].cell
+    }
+
+    func heightForRow(row: Int) -> CGFloat {
+        return cells[row].height
+    }
+
+    mutating func insertCellItem(item: TablViewCellItem, atIndex index: Int) {
+        cells.insert(item, atIndex: index)
+    }
+
+    mutating func removeCellItemAtIndex(index: Int) {
+        cells.removeAtIndex(index)
+    }
+}
+
+// MARK: StaticTableViewController
 class StaticTableViewController: UIViewController {
 
     let tableView: UITableView = UITableView(frame: CGRectZero, style: .Grouped)
 
-    var cells: [[TablViewCellItem]] = [[TablViewCellItem]]()
+    var sections: [TableViewSectionItem] = [TableViewSectionItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,43 +91,73 @@ class StaticTableViewController: UIViewController {
         view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: tableView, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1.0, constant: 0))
     }
+
+    func setupWithSections(sections: [TableViewSectionItem]) {
+        self.sections.appendContentsOf(sections)
+    }
 }
 
+// MARK: UITableViewDataSource
 extension StaticTableViewController: UITableViewDataSource {
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return cells.count
+        return sections.count
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cells[section].count
+        return sections[section].cellCount()
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return cells[indexPath.section][indexPath.row].cell
+        return sections[indexPath.section].cellForRow(indexPath.row)
     }
 
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return cells[indexPath.section][indexPath.row].height
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].headerTitle
+    }
+
+    func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return sections[section].footerTitle
     }
 }
 
+// MARK: UITableViewDelegate
 extension StaticTableViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return sections[indexPath.section].heightForRow(indexPath.row)
+    }
+
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return sections[section].headerHeight
+    }
+
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return sections[section].footerHeight
+    }
+
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let item = cells[indexPath.section][indexPath.row]
+        let item = sections[indexPath.row].cellItemForRow(indexPath.row)
         guard let target = item.target else { return }
         if target.respondsToSelector(item.action) {
             target.performSelector(item.action, withObject: item.cell)
         }
     }
+
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return sections[section].headerView
+    }
+
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return sections[section].footerView
+    }
 }
 
-// Cell insertion and deletion
+// MARK: Cell insertion and deletion
 extension StaticTableViewController {
 
     func indexPathForCellItem(cellItem: TablViewCellItem) -> NSIndexPath? {
-        for var section = 0; section < cells.count; section++ {
-            for var row = 0; row < cells[section].count; row++ {
-                if cells[section][row] == cellItem {
+        for var section = 0; section < sections.count; section++ {
+            for var row = 0; row < sections[section].cells.count; row++ {
+                if sections[section].cellItemForRow(row) == cellItem {
                     return NSIndexPath(forRow: row, inSection: section)
                 }
             }
@@ -89,40 +166,33 @@ extension StaticTableViewController {
     }
 
     func insertCell(cellItem: TablViewCellItem, atIndexPath indexPath: NSIndexPath, withRowAnimation rowAnimation: UITableViewRowAnimation = .Automatic) {
-        cells[indexPath.section].insert(cellItem, atIndex: indexPath.row)
+        sections[indexPath.section].insertCellItem(cellItem, atIndex: indexPath.row)
         tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: rowAnimation)
     }
 
-    func insertCells(cellItems: [[TablViewCellItem]], atIndexpaths indexPaths: [NSIndexPath], withRowAnimation rowAnimation: UITableViewRowAnimation = .Automatic) {
-        for indexPath in indexPaths {
-            cells[indexPath.section].insert(cellItems[indexPath.section][indexPath.row], atIndex: indexPath.row)
-        }
-        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: rowAnimation)
-    }
-
     func deleteCellWithIndexPath(indexPath: NSIndexPath, withRowAnimation rowAnimation: UITableViewRowAnimation = .Automatic) {
-        cells[indexPath.section].removeAtIndex(indexPath.row)
-        if cells[indexPath.section].count == 0 {
-            cells.removeAtIndex(indexPath.section)
+        sections[indexPath.section].removeCellItemAtIndex(indexPath.row)
+        if sections[indexPath.section].cellCount() == 0 {
+            sections.removeAtIndex(indexPath.section)
         }
         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: rowAnimation)
     }
 
     func moveRowAtIndexPath(indexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-        let fromCellItem = cells[indexPath.section][indexPath.row]
+        let fromCellItem = sections[indexPath.section].cellItemForRow(indexPath.row)
         if indexPath.section == toIndexPath.section {
             if indexPath.row < toIndexPath.row {
-                cells[indexPath.section].insert(fromCellItem, atIndex: toIndexPath.row + 1)
-                cells[indexPath.section].removeAtIndex(indexPath.row)
+                sections[indexPath.section].insertCellItem(fromCellItem, atIndex: toIndexPath.row + 1)
+                sections[indexPath.section].removeCellItemAtIndex(indexPath.row)
             } else if indexPath.row > toIndexPath.row {
-                cells[indexPath.section].insert(fromCellItem, atIndex: toIndexPath.row)
-                cells[indexPath.section].removeAtIndex(indexPath.row + 1)
+                sections[indexPath.section].insertCellItem(fromCellItem, atIndex: toIndexPath.row)
+                sections[indexPath.section].removeCellItemAtIndex(indexPath.row + 1)
             } else {
                 return
             }
         } else {
-            cells[toIndexPath.section].insert(fromCellItem, atIndex: toIndexPath.row)
-            cells[indexPath.section].removeAtIndex(indexPath.row)
+            sections[toIndexPath.section].insertCellItem(fromCellItem, atIndex: toIndexPath.row)
+            sections[indexPath.section].removeCellItemAtIndex(indexPath.row)
         }
 
         tableView.moveRowAtIndexPath(indexPath, toIndexPath: toIndexPath)
--- LINStaticTableViewController/TableViewController.swift
+++ LINStaticTableViewController/TableViewController.swift
@@ -55,24 +55,34 @@ class TableViewController: StaticTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "add")
-        cells.append([TablViewCellItem(cell: cell1, height: 44),
-                      TablViewCellItem(cell: cell2, height: 44),
-                      TablViewCellItem(cell: cell3, height: 44)])
 
-        cells.append([TablViewCellItem(cell: cell4, height: 44),
-                      TablViewCellItem(cell: cell5, height: 44),
-                      TablViewCellItem(cell: cell6, height: 44),
-                      TablViewCellItem(cell: cell7, height: 44)])
+        var section1 = TableViewSectionItem()
+//        section1.headerTitle = "section1's header"
+        section1.headerHeight = 0.1
+//        section1.footerTitle = "section1's footer"
+        section1.appendCellItems([TablViewCellItem(cell: cell1, height: 44),
+                                  TablViewCellItem(cell: cell2, height: 44),
+                                  TablViewCellItem(cell: cell3, height: 44)])
+
+        var section2 = TableViewSectionItem()
+        section2.headerTitle = "section2's header"
+        section2.footerTitle = "section2's footer"
+        section2.appendCellItems([TablViewCellItem(cell: cell4, height: 44),
+                                  TablViewCellItem(cell: cell5, height: 44),
+                                  TablViewCellItem(cell: cell6, height: 44),
+                                  TablViewCellItem(cell: cell7, height: 44)])
+
+        setupWithSections([section1, section2])
     }
 
     var count = 10
     func add() {
-        moveRowAtIndexPath(NSIndexPath(forRow: 2, inSection: 0), toIndexPath: NSIndexPath(forRow: 0, inSection: 1))
-//        let cell = UITableViewCell()
-//        cell.textLabel?.text = "cell\(count)"
-//        count++
-////        cells[0].append(TablViewCellItem(cell: cell, height: 44))
-////        tableView.reloadData()
-//        insertCell(TablViewCellItem(cell: cell, height: 44), atIndexPath: NSIndexPath(forRow: 2, inSection: 0))
+//        moveRowAtIndexPath(NSIndexPath(forRow: 2, inSection: 0), toIndexPath: NSIndexPath(forRow: 0, inSection: 1))
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "cell\(count)"
+        count++
+//        cells[0].append(TablViewCellItem(cell: cell, height: 44))
+//        tableView.reloadData()
+        insertCell(TablViewCellItem(cell: cell, height: 44), atIndexPath: NSIndexPath(forRow: 2, inSection: 0))
     }
 }
